b3f16b0dc84010edc6f0f59ba8a8b364d6314b1f
Kevin Tun <kevintun01@gmail.com>
2020-05-20 11:42:11 -0500

    added project link to itinerary-app

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -6,25 +6,31 @@ export const Home = () => {
         <React.Fragment>
             <Section>
                 <List header={'Contact'} list={[
-                    <a href="https://www.linkedin.com/in/kevin-tun/"> Linkedin </a>,
                     <a href="https://www.github.com/ktun95/"> Github </a>,
                     <a href="mailto:kevintun01@gmail.com"> Email </a>,
                     <a href="/KEVINTUNRESUME.pdf"> Resume </a>,
+                    <a href="https://www.linkedin.com/in/kevin-tun/"> Linkedin </a>,
 
                 ]} />
             </Section>
             <Section>
                 <Article
                     header={'Projects'}
-                    content={[<ProjectLink 
+                    content={[
+                            <ProjectLink 
                                 title={'monkey-memory'}
                                 link={'https://monkey-memory.herokuapp.com/'}
                                 description={'Implementation of a memory test like those used in studies of the working memory of chimpanzees. Built with React'} />,
-                             <ProjectLink 
+                            <ProjectLink 
                                 title={'mineralogie'}
                                 link={'https://monkey-memory.herokuapp.com/'}
-                                description={'Mock e-commerce site for purchasing rocks'} />]
-                            }
+                                description={'Mock e-commerce site for purchasing rocks'} />,
+                            <ProjectLink 
+                                title={'itinerary-temporary-name-app'}
+                                link={'https://tranquil-headland-56071.herokuapp.com/'}
+                                description={'Plan a trip filled with activities! See them appear on the map! Very WIP!'} />
+
+                            ]}
                 />
                 {/* <Article
                     header={'Projects'}
